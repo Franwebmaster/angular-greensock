141ddcdddcb4cd2d6f6173e536b04456426d27e6
Development <franciscobarros754@gmail.com>
2019-01-25 12:18:49 -0300

    add timelinemax effect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 
 
+import { TimelineMax, Power2, Expo } from 'gsap';
 import TweenMaxModule from "gsap/TweenMax";
-import { TimelineMax } from 'gsap';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +12,9 @@ import { TimelineMax } from 'gsap';
 export class AppComponent {
 
   @ViewChild('btnAnimated') btnAnimated: ElementRef;
+  @ViewChild('backgroundText') backgroundText: ElementRef;
+  @ViewChild('containerOverlay') containerOverlay: ElementRef;
+  @ViewChild('containerOverlayTwo') containerOverlayTwo: ElementRef;
 
   animated():void {
 
@@ -21,7 +24,22 @@ export class AppComponent {
       y: -100,
       opacity: 0
     })
-
+    .to(this.backgroundText.nativeElement, 2, {
+      y: -400,
+      opacity: 0,
+      ease: Power2.easeInOut,
+    }, 2)
+    .from(this.containerOverlay.nativeElement, 2, {
+      ease: Power2.easeInOut
+    })
+    .to(this.containerOverlay.nativeElement, 2, {
+      top: "-110%",
+      ease: Expo.easeInOut
+    }, 2.6)
+    .to(this.containerOverlayTwo.nativeElement, 2, {
+      top: "-110%",
+      ease: Expo.easeInOut
+    }, 3)
   }
   
 }
